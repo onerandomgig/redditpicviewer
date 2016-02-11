be3463f05b71d0b2515307684bb41901f35c93d1
Prashanth Reddy <prashanth@peoplecloudlabs.com>
2016-02-11 00:12:50 -0800

    Extracting Slideshow controls into a separate class (outside of the slideshow activity)

--- app/src/main/java/in/continuousloop/redditpicviewer/MusicSlideshowActivity.java
+++ app/src/main/java/in/continuousloop/redditpicviewer/MusicSlideshowActivity.java
@@ -1,48 +1,24 @@
 package in.continuousloop.redditpicviewer;
 
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import in.continuousloop.redditpicviewer.api.APIManager;
 import in.continuousloop.redditpicviewer.constants.AppConstants;
-import in.continuousloop.redditpicviewer.listeners.PicSubredditResponseListener;
+import in.continuousloop.redditpicviewer.listeners.NetworkImageSlideshowPlayer;
 import in.continuousloop.redditpicviewer.model.MusicTrackItem;
-import in.continuousloop.redditpicviewer.model.SubredditPicItem;
-import in.continuousloop.redditpicviewer.model.SubredditPicsWrapper;
 import in.continuousloop.redditpicviewer.model.SubredditSection;
 
 /**
  * This activity displays a list of images while playing a music track in the background.
  */
-public class MusicSlideshowActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
+public class MusicSlideshowActivity extends AppCompatActivity {
 
-    private String mNextPicsPageTag;
-
-    private boolean isPlaying;
-
-    private SubredditSection mCurrentSection;
-    private List<SubredditPicItem> imageList;
-    private MusicTrackItem musicTrackItem;
-
-    private SimpleDraweeView mImageView;
-    private TextView mTitleView;
-
-    private MediaPlayer audioPlayer;
-
-    private Thread mSlideshowThread;
+    private NetworkImageSlideshowPlayer slideshowPlayer;
 
     public void onCreate(final Bundle savedState) {
         super.onCreate(savedState);
@@ -52,14 +28,19 @@ public class MusicSlideshowActivity extends AppCompatActivity implements MediaPl
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
-        mImageView = (SimpleDraweeView) findViewById(R.id.imageView);
-        mTitleView = (TextView) findViewById(R.id.title);
+        // Image view and title view to show the image and title
+        SimpleDraweeView lImageView = (SimpleDraweeView) findViewById(R.id.imageView);
+        TextView lTitleView = (TextView) findViewById(R.id.title);
 
-        imageList = new ArrayList<>();
-        musicTrackItem = (MusicTrackItem) getIntent().getSerializableExtra(AppConstants.Extras.CURRENT_SELECTED_TRACK);
+        // The music track to play during the slide show
+        MusicTrackItem musicTrackItem = (MusicTrackItem) getIntent().getSerializableExtra(AppConstants.Extras.CURRENT_SELECTED_TRACK);
 
+        // The current tab that is selected (to fetch the images to show in the slide show)
         int lCurrentSelectedTab = getIntent().getIntExtra(AppConstants.Extras.CURRENT_SELECTED_TAB, 0);
-        mCurrentSection = APIManager.getInstance().getSubredditSections().get(lCurrentSelectedTab);
+        SubredditSection lCurrentSection = APIManager.getInstance().getSubredditSections().get(lCurrentSelectedTab);
+
+        // The slide show player instance.
+        slideshowPlayer = new NetworkImageSlideshowPlayer(this, lImageView, lTitleView, musicTrackItem, lCurrentSection);
     }
 
     /**
@@ -67,23 +48,14 @@ public class MusicSlideshowActivity extends AppCompatActivity implements MediaPl
      */
     public void onResume() {
         super.onResume();
-        if (imageList.size() == 0) {
-            _fetchImages(true);
-        } else {
-            _startSlideshow();
-        }
+        slideshowPlayer.startSlideshow();
     }
 
     /**
      * {@inheritDoc}
      */
     public void onPause() {
-        audioPlayer.release();
-        audioPlayer = null;
-
-        isPlaying = false;
-        mSlideshowThread.interrupt();
-
+        slideshowPlayer.stopSlideshow();
         super.onPause();
     }
 
@@ -91,110 +63,7 @@ public class MusicSlideshowActivity extends AppCompatActivity implements MediaPl
      * {@inheritDoc}
      */
     public void onStop() {
-
-        if (audioPlayer != null) {
-            audioPlayer.release();
-            audioPlayer = null;
-            mSlideshowThread.interrupt();
-        }
-
+        slideshowPlayer.stopSlideshow();
         super.onStop();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onPrepared(MediaPlayer mp) {
-        audioPlayer.start();
-    }
-
-    /**
-     * Fetch the images for the current section and start the slideshow after the images are fetched
-     *
-     * @param startSlideShow - True to start, False otherwise.
-     */
-    private void _fetchImages(final boolean startSlideShow) {
-        APIManager.getInstance().getPicsSubredditData(mCurrentSection, mNextPicsPageTag, new PicSubredditResponseListener() {
-            @Override
-            public void handleSuccess(SubredditPicsWrapper picsResponse) {
-                mNextPicsPageTag = picsResponse.getNext();
-                imageList.addAll(picsResponse.getPictures());
-
-                if (startSlideShow) {
-                    _startSlideshow();
-                }
-            }
-
-            @Override
-            public void handleError(String aErrorMsg) {
-                Toast.makeText(getApplicationContext(), aErrorMsg, Toast.LENGTH_LONG);
-            }
-        });
-    }
-
-    /**
-     * Start the slide show and switch the image every 2 seconds. As the slideshow reaches the end
-     * of the list (last 5 images), a new set of images is fetched. If there are no more images,
-     * the slideshow loops back to the first picture.
-     */
-    private void _startSlideshow() {
-
-        try {
-            _initMediaPlayer();
-
-            audioPlayer.setDataSource(musicTrackItem.getUrl());
-            audioPlayer.prepareAsync();
-
-            isPlaying = true;
-        } catch (IOException e) {
-            Log.e(MusicSlideshowActivity.class.getName(), "_startSlideshow: Failed to play audio track for slideshow");
-        }
-
-        mSlideshowThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                int lCurrentImgIdx = 0;
-                try {
-                    while (isPlaying) {
-
-                        final SubredditPicItem lPicItem = imageList.get(lCurrentImgIdx);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-
-                                // Image view
-                                mImageView.setImageURI(Uri.parse(lPicItem.getImageUrlWithResolution(0, 0)));
-
-                                // Title view
-                                mTitleView.setText(lPicItem.getTitle());
-                            }
-                        });
-                        Thread.sleep(2000);
-                        lCurrentImgIdx++;
-
-                        if (lCurrentImgIdx == imageList.size() - 5) {
-                            _fetchImages(false);
-                        } else if (lCurrentImgIdx >= imageList.size()) {
-                            lCurrentImgIdx = 0;
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    Log.i(MusicSlideshowActivity.class.getName(), "_startSlideshow: Slideshow interrupted.");
-                }
-            }
-        });
-        mSlideshowThread.start();
-    }
-
-    /**
-     * Initialize the audio player.
-     */
-    private void _initMediaPlayer() {
-        audioPlayer = new MediaPlayer();
-        audioPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        audioPlayer.setLooping(true);
-        audioPlayer.setOnPreparedListener(this);
     }
 }
---
+++ app/src/main/java/in/continuousloop/redditpicviewer/listeners/NetworkImageSlideshowPlayer.java
@@ -0,0 +1,251 @@
+package in.continuousloop.redditpicviewer.listeners;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Animatable;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.image.ImageInfo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import in.continuousloop.redditpicviewer.api.APIManager;
+import in.continuousloop.redditpicviewer.model.MusicTrackItem;
+import in.continuousloop.redditpicviewer.model.SubredditPicItem;
+import in.continuousloop.redditpicviewer.model.SubredditPicsWrapper;
+import in.continuousloop.redditpicviewer.model.SubredditSection;
+
+/**
+ * The class to start and stop the slideshow. This class also fetches the images that need to play
+ * during the slideshow
+ */
+public class NetworkImageSlideshowPlayer implements MediaPlayer.OnPreparedListener {
+
+    private String mNextPicsPageTag;
+
+    private boolean isPlaying;
+
+    private SubredditSection mCurrentSection;
+    private MusicTrackItem musicTrackItem;
+
+    private SimpleDraweeView mImageView;
+    private TextView mTitleView;
+
+    private List<SubredditPicItem> imageList;
+
+    private MediaPlayer audioPlayer;
+
+    private Thread mSlideshowThread;
+
+    private Object mImageFetchLock;
+    private Context mContext;
+
+    /**
+     * Create an instance of the NetworkImageSlideshowPlayer
+     *
+     * @param aContext        - The activity that displays the slideshow
+     * @param draweeView      - The imageview to display the image
+     * @param titleView       - The textview to display the title
+     * @param aMusicTrackItem - The music track that should be played during the slideshow
+     * @param aCurrentSection - The section to fetch the images from (HOT, NEW, RISING)
+     */
+    public NetworkImageSlideshowPlayer(Context aContext,
+                                       SimpleDraweeView draweeView,
+                                       TextView titleView,
+                                       MusicTrackItem aMusicTrackItem,
+                                       SubredditSection aCurrentSection) {
+
+        mContext = aContext;
+
+        mImageView = draweeView;
+        mTitleView = titleView;
+
+        musicTrackItem = aMusicTrackItem;
+        mCurrentSection = aCurrentSection;
+
+        mImageFetchLock = new Object();
+        imageList = new ArrayList<>();
+    }
+
+    /**
+     * Fetch the images and start the slideshow.
+     */
+    public void startSlideshow() {
+        if (imageList.size() == 0) {
+            _fetchImages(true);
+        } else {
+            _startSlideshow();
+        }
+    }
+
+    /**
+     * Stop the slideshow
+     */
+    public void stopSlideshow() {
+
+        if (audioPlayer != null) {
+            audioPlayer.release();
+            audioPlayer = null;
+
+            isPlaying = false;
+            mSlideshowThread.interrupt();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        audioPlayer.start();
+    }
+
+    /**
+     * Fetch the images for the current section and start the slideshow after the images are fetched
+     *
+     * @param startSlideShow - True to start, False otherwise.
+     */
+    private void _fetchImages(final boolean startSlideShow) {
+        APIManager.getInstance().getPicsSubredditData(mCurrentSection, mNextPicsPageTag, new PicSubredditResponseListener() {
+            @Override
+            public void handleSuccess(SubredditPicsWrapper picsResponse) {
+                mNextPicsPageTag = picsResponse.getNext();
+                imageList.addAll(picsResponse.getPictures());
+
+                if (startSlideShow) {
+
+                    DraweeController controller = Fresco.newDraweeControllerBuilder()
+                            .setControllerListener(_getControllerListener())
+                            .setUri(imageList.get(0).getImageUrlWithResolution(0, 0))
+                            .build();
+                    mImageView.setController(controller);
+
+                    _startSlideshow();
+                }
+            }
+
+            @Override
+            public void handleError(String aErrorMsg) {
+                Toast.makeText(mContext, aErrorMsg, Toast.LENGTH_LONG);
+            }
+        });
+    }
+
+    /**
+     * Start the slide show and switch the image every 2 seconds. As the slideshow reaches the end
+     * of the list (last 5 images), a new set of images is fetched. If there are no more images,
+     * the slideshow loops back to the first picture.
+     */
+    private void _startSlideshow() {
+
+        try {
+            _initMediaPlayer();
+
+            audioPlayer.setDataSource(musicTrackItem.getUrl());
+            audioPlayer.prepareAsync();
+
+            isPlaying = true;
+        } catch (IOException e) {
+            Log.e(NetworkImageSlideshowPlayer.class.getName(), "_startSlideshow: Failed to play audio track for slideshow");
+        }
+
+        mSlideshowThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                int lCurrentImgIdx = 0;
+                try {
+                    while (isPlaying) {
+
+                        final SubredditPicItem lPicItem = imageList.get(lCurrentImgIdx);
+                        ((Activity) mContext).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                // Image view
+                                mImageView.setImageURI(Uri.parse(lPicItem.getImageUrlWithResolution(0, 0)));
+
+                                // Title view
+                                mTitleView.setText(lPicItem.getTitle());
+                            }
+                        });
+
+                        // Wait maximum 10s.
+                        synchronized (mImageFetchLock) {
+                            mImageFetchLock.wait(5000);
+                        }
+
+                        lCurrentImgIdx++;
+
+                        if (lCurrentImgIdx == imageList.size() - 5) {
+                            _fetchImages(false);
+                        } else if (lCurrentImgIdx >= imageList.size()) {
+                            lCurrentImgIdx = 0;
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    Log.i(NetworkImageSlideshowPlayer.class.getName(), "_startSlideshow: Slideshow interrupted.");
+                }
+            }
+        });
+        mSlideshowThread.start();
+    }
+
+    /**
+     * Initialize the audio player.
+     */
+    private void _initMediaPlayer() {
+        audioPlayer = new MediaPlayer();
+        audioPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        audioPlayer.setLooping(true);
+        audioPlayer.setOnPreparedListener(this);
+    }
+
+    /**
+     * Get the {@link ControllerListener} to track image download events.
+     * @return {@link ControllerListener}
+     */
+    private ControllerListener _getControllerListener() {
+        ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
+            @Override
+            public void onFinalImageSet(
+                    String id,
+                    @Nullable ImageInfo imageInfo,
+                    @Nullable Animatable anim) {
+                Log.d(NetworkImageSlideshowPlayer.class.getName(), "onFinalImageSet: Image downloaded");
+                synchronized (mImageFetchLock) {
+                    mImageFetchLock.notifyAll();
+                }
+            }
+
+            @Override
+            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+                synchronized (mImageFetchLock) {
+                    mImageFetchLock.notifyAll();
+                }
+            }
+
+            @Override
+            public void onFailure(String id, Throwable throwable) {
+                synchronized (mImageFetchLock) {
+                    mImageFetchLock.notifyAll();
+                }
+            }
+        };
+
+        return controllerListener;
+    }
+}
